dcbcecef32b05b7b1c9cc2fd22c27aa589da2be2
Traspo <nyaspo8@gmail.com>
2024-02-21 13:55:51 -0500

    starting code for portfolio

---
+++ src/pages/AboutMePage.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Me: Natalie Yaspo</h1>
+        <p>
+          Need to enter information here
+        </p>
+      </div>
+    );
+  }
