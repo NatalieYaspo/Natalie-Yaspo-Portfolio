92fb969a3daf8b0b1ee65601dfcdeb63ad08c8f1
Traspo <nyaspo8@gmail.com>
2024-02-22 15:15:25 -0500

    resume and contact pages added

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,46 @@
 export default function Contact() {
-    return (
-      <div>
-        <h2>Contact Me:</h2>
-        <p>
-          Need to enter information here
-        </p>
-      </div>
-    );
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isGoing: true,
+  //     numberOfGuests: 2
+  //   };
+
+  //   this.handleInputChange = this.handleInputChange.bind(this);
+  // }
+
+  // handleInputChange(event) {
+  //   const target = event.target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   const name = target.name;
+
+  //   this.setState({
+  //     [name]: value
+  //   });
+
+  return (
+    <div>
+      <h2>Contact Me:</h2>
+      <form>
+      <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Name:</label>
+          <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Name Goes Here..." />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Email address:</label>
+          <input type="email" 
+          className="form-control" 
+          id="exampleFormControlInput1" 
+          placeholder="name@example.com..." />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">Message:</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+
+
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,9 +2,21 @@ export default function Resume() {
     return (
       <div>
         <h2>Resume:</h2>
-        <p>
-          Need to enter information here
-        </p>
+        <a href="https://drive.google.com/file/d/1MkTFZ-mYZQ9PE0CZNkJXTc_sc-Y67_Fi/view?usp=sharing">Download Resume</a>
+        <div>
+          <h3>Proficiencies:</h3>
+          <ul>
+            <li>Technical Skills:</li>
+            <ul>
+              <li>Front End: HTML5, CSS, JQuery, Javascript, Bootstrap, AJAX, SASS</li>
+              <li>Back End: MySQL, MongoDB, Express, ReactJS, Node, Handlebars, Webpack, MERN, GraphQl</li>
+            </ul>
+            <li>Non-Technical Skills</li>
+            <ul>
+              <li>Strong Communication, Customer Service Experience, Teamwork, Highly Organized, Strong Leadership and Project Management</li>
+            </ul>
+          </ul>
+        </div>
       </div>
     );
   }
