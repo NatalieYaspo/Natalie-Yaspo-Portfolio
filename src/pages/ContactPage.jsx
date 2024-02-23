af8af890ca3dca875f02508e1d6d657529a7f4ac
Traspo <nyaspo8@gmail.com>
2024-02-23 10:07:29 -0500

    alerts working for form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,46 +1,74 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+// import ReactDOM from "react-dom";
+import { ErrorMessage } from "@hookform/error-message";
+
 export default function Contact() {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     isGoing: true,
-  //     numberOfGuests: 2
-  //   };
-
-  //   this.handleInputChange = this.handleInputChange.bind(this);
-  // }
-
-  // handleInputChange(event) {
-  //   const target = event.target;
-  //   const value = target.type === 'checkbox' ? target.checked : target.value;
-  //   const name = target.name;
-
-  //   this.setState({
-  //     [name]: value
-  //   });
+  const { register, formState: { errors }, handleSubmit } = useForm({
+    criteriaMode: "all"
+  });
+  const [data, setData] = useState("");
+  const onSubmit = (data) => console.log(data)
 
   return (
     <div>
       <h2>Contact Me:</h2>
-      <form>
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
       <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label">Name:</label>
-          <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Name Goes Here..." />
+          <label htmlFor="exampleFormControlInput1" className="form-label">Name:</label>
+          <input onBlur={() => {
+            alert("Name is required.");
+            register("name", {
+            required: "Name is required.",
+          })}} type="name" className="form-control" id="exampleFormControlInput1" placeholder="Name Goes Here..." />
         </div>
         <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label">Email address:</label>
+          <label htmlFor="exampleFormControlInput1" className="form-label">Email address:</label>
           <input type="email" 
           className="form-control" 
           id="exampleFormControlInput1" 
-          placeholder="name@example.com..." />
+          placeholder="name@example.com..."
+          onBlur={() => {
+            // alert("Email is required.");
+            register("email", { 
+            required: "Email Address is required.",
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              message: "Must enter a valid email address."}
+            })}} />
         </div>
         <div className="mb-3">
-          <label for="exampleFormControlTextarea1" className="form-label">Message:</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <label htmlFor="message" className="form-label">Message:</label>
+          <textarea onBlur={() => {
+            alert("Please enter a message.");
+            register("message", { 
+            required: "Please enter a message.",
+            }
+          )}} className="form-control" id="message" rows="3"></textarea>
         </div>
+        <button type="submit" className="btn btn-sm btn-outline-secondary">SUBMIT</button>
+        {/* Showing the error messages when incorrect information entered in form. */}
+        <ErrorMessage
+        errors={errors}
+        name="multipleErrorInput"
+        render={({ messages }) => {
+          console.log("messages", messages);
+          return messages
+            ? Object.entries(messages).map(([type, message]) => (
+                <p key={type}>{message}</p>
+              ))
+            : null;
+        }}
+      />
       </form>
+      <br></br>
+      <button type="button" className="btn btn-sm btn-outline-secondary"><a href="mailto:nyaspo8@gmail.com">Email Me</a></button>
     </div>
   );
 }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 
 
 
